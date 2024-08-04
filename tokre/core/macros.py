39021aa2203e7eb48d1b0d4d17ab656811e47a64
Noa Nabeshima <noanabeshima@gmail.com>
2024-08-04 13:40:29 -0700

    Add some macros

--- tokre/core/macros.py
+++ tokre/core/macros.py
@@ -51,6 +51,59 @@ class VarPrefix(nn.Module):
     @property
     def pyregex(self):
         return r".{0," + str(self.max_len) + r"}"
+    
+import regex as re
+
+class TokRegex(nn.Module):
+    def __init__(self, pattern, search=False):
+        super().__init__()
+        self.name = f"TokRegex:{pattern}:{randstr()}"
+        self.pattern = pattern
+        self.search = search
+    
+    def matches(self, toks, partial, reversed):
+        if partial.end == len(toks) and not reversed:
+            return []
+        
+        if partial.end == len(toks) and reversed:
+            return []
+        
+        tok = toks[partial.end]
+
+        if (self.search and re.search(self.pattern, tok)) or re.fullmatch(self.pattern, tok):
+            return [
+                PartialMatch(
+                    name=self.name,
+                    start=partial.end,
+                    end=partial.end + 1,
+                    defns=partial.defns,
+                    data=None
+                )
+            ]
+        else:
+            return []
+
+# class Prefix(nn.Module):
+#     def __init__(self, child_module):
+#         super().__init__()
+#         self.name = f"Prefix:{randstr()}"
+#         self.child_module = child_module
+    
+#     def matches(self, toks, partial, reversed):
+#         matches = self.child_module.matches(toks, partial, reversed)
+#         res = []
+#         for match in matches:
+#             for i in range(match.start, match.end):
+#                 res.append(
+#                     PartialMatch(
+#                         name=self.name,
+#                         start=match.start,
+#                         end=i,
+#                         defns=match.defns,
+#                         data=match.data
+#                     )
+#                 )
+#         return res
 
 
-DEFINED_MACROS = {"var_prefix": VarPrefix}
+DEFINED_MACROS = {"var_prefix": VarPrefix, "re": TokRegex}
